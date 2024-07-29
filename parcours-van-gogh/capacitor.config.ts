2dde6ff1321588579ad171bab1facc0a4972ec3a
Baptiste P. <63961483+BaptisteP31@users.noreply.github.com>
2024-07-30 00:02:39 +0200

    Removes refresher

--- parcours-van-gogh/capacitor.config.ts
+++ parcours-van-gogh/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'parcours.van.gogh',
-  appName: 'parcours-van-gogh',
+  appId: 'io.ionic.starter',
+  appName: 'list',
   webDir: 'www'
 };
 
